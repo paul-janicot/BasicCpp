542ff2c94d31b3430b6bf2800905dbf22df06d1c
Paul Janicot <paul.janicot@artfx.fr>
2025-03-24 16:19:05 +0100

    [add] until class abilities

--- Basic C++/Basic C++/Basic C++.cpp
+++ Basic C++/Basic C++/Basic C++.cpp
@@ -1,148 +1,156 @@
-// Basic C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//// Basic C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
+////
 //
-
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-using std::cout;
-using std::endl;
-using std::cin;
-
-using std::string;
-
-//Function
-
-void Choice(string firstChoice, string secondChoice, string thirdChoice) {
-
-    cout << "1: " << firstChoice << " 2: " << secondChoice << " 3 : " << thirdChoice << endl;
-
-    int classe = 0;
-    cin >> classe;
-
-    switch (classe)
-    {
-    case 1:
-        cout << firstChoice;
-        break;
-    case 2:
-        cout << secondChoice;
-        break;
-    case 3:
-        cout << thirdChoice;
-        break;
-    default:
-        cout << "Not valid" << endl;
-        break;
-    }
-
-}
-
-
-int main()
-{
-
-    srand(time(NULL));
-    
-    string narratorName = "The narrator";
-
-    cout << "Hello I am" << narratorName << "\n and you ?" << endl;
-
-    string playerName;
-   // cin >> playerName;
-
-    cout << playerName;
-
-
-    int firstDice = ((std::rand() % 6) + 1);
-    cout << firstDice;
-
-    cout << &firstDice;
-
-    string bagOfLoot[5]{ "pen","apple" };
-    cout << bagOfLoot[0];
-
-    std::vector<string>myVector(5, "pipou");
-    cout << myVector[0] << endl;
-    myVector.push_back("loulou");
-    cout << myVector[myVector.size() - 1];
-
-    std::map<string, int>abilities
-    {
-        {"strength", 0},
-        {"intelligence", 1},
-        {"Dexterity", 0}
-    };
-
-    cout << abilities["strength"];
-
-    int choice = 0;
-
-    cout << "1: Thief, 2: Warrior, 3 : Wizard";
-    //cin >> choice;
-
-    switch (choice)
-    {
-    case 1:
-        cout << "Thief";
-        break;
-    case 2:
-        cout << "Warrior";
-        break;
-    case 3:
-        cout << "Wizard";
-        break;
-    default:
-        cout << "Not valid" << endl;
-        break;
-    }
-
-
-
- 
-
-
-
-
-
-
-    for (string i: bagOfLoot) {
-        cout << i << endl;
-    }
-    for (int i = 0; i < 3; i++) {
-        cout << bagOfLoot[i] << endl;
-    }
-
-    for (std::pair<const string, int>& ability : abilities) {
-       // cout << ability.first << ":" << ability.second << endl;
-    }
-
-    int answer = 0;
-    do {
-        
-        cout << "Enter 1";
-        cin >> answer;
-        
-    } while (answer != 1);
-
-
-    Choice("A", "B", "X");
-
-
-    return 0;
-}
-
-
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//#include <map>
+//using std::cout;
+//using std::endl;
+//using std::cin;
+//
+//using std::string;
+//
+////Function
+//
+//void Choice(string firstChoice, string secondChoice, string thirdChoice) {
+//
+//    cout << "1: " << firstChoice << " 2: " << secondChoice << " 3 : " << thirdChoice << endl;
+//
+//    int classe = 0;
+//    cin >> classe;
+//
+//    switch (classe)
+//    {
+//    case 1:
+//        cout << firstChoice;
+//        break;
+//    case 2:
+//        cout << secondChoice;
+//        break;
+//    case 3:
+//        cout << thirdChoice;
+//        break;
+//    default:
+//        cout << "Not valid" << endl;
+//        break;
+//    }
+//
+//}
+//int Roll(int maxNumber) {
+//    return ((std::rand() % maxNumber) + 1);
+//
+//}
+//
+//
+//int main()
+//{
+//
+//    srand(time(NULL));
+//    
+//    cout << "Welcome to the tavern traveler ! \nplease choose a drink";
+//    Choice("Beer", "Apple Cider", "Wine");
+//
+//
+//        /* string narratorName = "The narrator";
+//
+//        cout << "Hello I am" << narratorName << "\n and you ?" << endl;
+//
+//        string playerName;
+//       // cin >> playerName;
+//
+//        cout << playerName;
+//
+//
+//        int firstDice = ((std::rand() % 6) + 1);
+//        cout << firstDice;
+//
+//        cout << &firstDice;
+//
+//        string bagOfLoot[5]{ "pen","apple" };
+//        cout << bagOfLoot[0];
+//
+//        std::vector<string>myVector(5, "pipou");
+//        cout << myVector[0] << endl;
+//        myVector.push_back("loulou");
+//        cout << myVector[myVector.size() - 1];
+//
+//        std::map<string, int>abilities
+//        {
+//            {"strength", 0},
+//            {"intelligence", 1},
+//            {"Dexterity", 0}
+//        };
+//
+//        cout << abilities["strength"];
+//
+//        int choice = 0;
+//
+//        cout << "1: Thief, 2: Warrior, 3 : Wizard";
+//        //cin >> choice;
+//
+//        switch (choice)
+//        {
+//        case 1:
+//            cout << "Thief";
+//            break;
+//        case 2:
+//            cout << "Warrior";
+//            break;
+//        case 3:
+//            cout << "Wizard";
+//            break;
+//        default:
+//            cout << "Not valid" << endl;
+//            break;
+//        }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//        for (string i: bagOfLoot) {
+//            cout << i << endl;
+//        }
+//        for (int i = 0; i < 3; i++) {
+//            cout << bagOfLoot[i] << endl;
+//        }
+//
+//        for (std::pair<const string, int>& ability : abilities) {
+//           // cout << ability.first << ":" << ability.second << endl;
+//        }
+//
+//        int answer = 0;
+//        do {
+//
+//            cout << "Enter 1";
+//            cin >> answer;
+//
+//        } while (answer != 1);
+//
+//
+//        Choice("A", "B", "X");
+//
+//        */
+//        return 0;
+//}
+//
+//
+//
+//
+//
+//// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+//// Debug program: F5 or Debug > Start Debugging menu
+//
+//// Tips for Getting Started: 
+////   1. Use the Solution Explorer window to add/manage files
+////   2. Use the Team Explorer window to connect to source control
+////   3. Use the Output window to see build output and other messages
+////   4. Use the Error List window to view errors
+////   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+////   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Basic C++/Basic C++/TavernAdventure.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <random>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+using std::string;
+using namespace std;
+
+//Function
+
+int Choice(string firstChoice, string secondChoice, string thirdChoice) {
+
+    while (true)
+    {
+    cout << "1: " << firstChoice << " 2: " << secondChoice << " 3 : " << thirdChoice << endl;
+
+    int classe = 0;
+    cin >> classe;
+
+    
+
+   
+    switch (classe)
+    {
+    case 1:
+        cout << firstChoice << endl;
+        return 1;
+        break;
+    case 2:
+        cout << secondChoice << endl;
+        return 2;
+        break;
+    case 3:
+        cout << thirdChoice << endl;
+        return 3;
+        break;
+    default:
+        cout << "Not valid" << endl;
+        continue;
+    }
+    }
+
+}
+int Roll(int minNumber,int maxNumber) {
+    random_device rd;
+    mt19937_64 gen(rd());
+    uniform_int_distribution<> distrib(minNumber, maxNumber);
+    return distrib(gen);
+
+}
+
+
+int main()
+{
+
+    srand(time(NULL));
+    int drink = 0;
+    bool isAsleep = false;
+    int money = Roll(100,200);
+    cout << "Welcome to the tavern traveler ! \nplease choose a drink" << endl << "You have " << money << " coins" << endl;
+    drink = Choice("Beer", "Apple Cider", "La gnole de Papi");
+
+    switch (drink)
+    {
+    case 1:
+        cout << "Nice choice traveler ! Here is your beer" << endl;
+        break;
+    case 2:
+        cout << "Ahah ! Here is your weak Apple Cider" << endl;
+        break;
+    case 3:
+        cout << "Damn good luck with that...";
+        break;
+        isAsleep = true;
+    default:
+        break;
+    }
+
+    if (isAsleep) {
+        money -= 50;
+        cout << "You fall asleep right after the first sip..." << endl << "To bad for you a fardadet malicieux just steal you " << 50 << " coins." << endl;
+        cout << "You have " << money << " golds left";
+        isAsleep = false;
+        
+    }
+
+    cout << "Drink Time is over... Go on an adventure now ! Who are you ?" << endl << "Enter a pseudo: " << endl;
+    string pseudo = "";
+    cin >> pseudo;
+    cout << "Well Hello " << pseudo << " ! You can choose your class now" << endl;
+    int classe = Choice("Warrior", "Thief", "Wizard");
+
+    std::map<string, int> abilities;
+
+    abilities["strength"] = 0;
+    abilities["inteligence"] = 0;
+    abilities["dexterity"] = 0;
+
+    switch (classe)
+    {
+    case 1:
+        cout << "You choose the Warrior !";
+        abilities["strength"] = Roll(15,20);
+        abilities["inteligence"] = Roll(0,10);
+        abilities["dexterity"] = Roll(10,15);
+        break;
+    case 2:
+        cout << "You choose the Thief !";
+        abilities["strength"] = Roll(10,15);
+        abilities["inteligence"] = Roll(10, 15);
+        abilities["dexterity"] = Roll(10, 15);
+        break;
+
+    case 3:
+        cout << "You choose the Wizard !";
+        abilities["strength"] = Roll(0,10);
+        abilities["inteligence"] = Roll(15, 20);
+        abilities["dexterity"] = Roll(10, 12);
+        break;
+
+
+    default:
+        break;
+    }
+
+    cout << "This is your abilities: " << endl;
+        for (std::pair<const string, int>& ability : abilities) {
+            cout << ability.first << ":" << ability.second << endl;
+        }
+    
+
+    return 0;
+}
